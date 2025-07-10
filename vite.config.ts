ad54d95b78b18511bfd249ee41e4b0f6a20bcf45
Karlo Gamboa <karlo.gamboa@outlook.com>
2025-07-10 11:32:07 -0600

    feat: set base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     port: 4000, // Cambia 4000 por el puerto que prefieras
